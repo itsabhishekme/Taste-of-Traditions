eb4f5e6dc89636138a9ed8f0aa399f8aeb3252dd
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-08 17:31:16 +0530

    App Build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.tasteoftraditions.app',
-  appName: 'taste-of-traditions',
-  webDir: 'public'
+  appId: "com.tasteoftraditions.app",
+  appName: "Taste of Traditions",
+  webDir: "out",
 };
 
 export default config;
