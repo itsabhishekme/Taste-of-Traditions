1fd4822fb821f2cab46d97275e0529730cd10d0b
Abhishek Shrivastava <abhishekkumardipu@gmail.com>
2026-05-08 13:58:37 +0530

    Android App

---
+++ android/app/src/main/java/com/tasteoftraditions/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tasteoftraditions.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tasteoftraditions.app',
+  appName: 'taste-of-traditions',
+  webDir: 'public'
+};
+
+export default config;
